d47b507cb89bef0eb0d7b15d3d2f84f5f826b407
Pantherr03 <paul.christoph.kern@gmail.com>
2021-02-01 21:45:55 +0100

    update

--- dasVerrLabyrinth/src/main/java/com/benpaul/dasverrlabyrinth/FinishController.java
+++ dasVerrLabyrinth/src/main/java/com/benpaul/dasverrlabyrinth/FinishController.java
@@ -7,6 +7,7 @@ package com.benpaul.dasverrlabyrinth;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -27,12 +28,18 @@ public class FinishController implements Initializable {
     private ImageView secondPlace;
     @FXML
     private ImageView thirdPlace;
+    
+    ArrayList<Integer> scores;
 
-    /**
-     * Initializes the controller class.
-     */
+   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        scores = new ArrayList();
+        scores.add(App.players[0].score);
+        scores.add(App.players[1].score);
+        scores.add(App.players[2].score);
+        scores.add(App.players[3].score);
+        
         Image firstPlaceImg;
         File firstPlaceFile = new File("src/main/resources/img/cardbackRed.png");
         firstPlaceImg = new Image(firstPlaceFile.toURI().toString());
@@ -41,5 +48,15 @@ public class FinishController implements Initializable {
     @FXML
     private void btnRestart(ActionEvent event) {
     }
+    
+    private int determinWinner(){
+        int largest = scores.get(0);
+        for(int x=0; x<4; x++)
+            if (scores.get(x) > largest)
+                largest = scores.get(x);
+        return largest;
+    }
+
+    
     
 }
