1d481c00d1aef640bbc83c1a5da8e67d43f7a227
Pantherr03 <paul.christoph.kern@gmail.com>
2021-02-09 18:05:31 +0100

    HomeView

--- dasVerrLabyrinth/src/main/java/com/benpaul/dasverrlabyrinth/HomeController.java
+++ dasVerrLabyrinth/src/main/java/com/benpaul/dasverrlabyrinth/HomeController.java
@@ -12,8 +12,6 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -21,33 +19,17 @@ import javafx.scene.image.ImageView;
  * @author benbartel
  */
 public class HomeController implements Initializable {
-
-    @FXML
-    private ImageView imgViewTest;
-
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        File file = new File("src/main/resources/all_tiles/poltergeist.png");
-        Image image = new Image(file.toURI().toString());
-        imgViewTest.setImage(image);
         // TODO
     }    
 
     @FXML
-    private void btnOof(ActionEvent event) throws IOException {
+    private void btnStartGame(ActionEvent event) throws IOException {
         App.setRoot("gameView");
-        /*File file = new File("src/main/resources/all_tiles/corner.png");
-        Image image = new Image(file.toURI().toString());
-        imgViewTest.setImage(image);*/
     }
 
-    @FXML
-    private void testRot(ActionEvent event) {
-        imgViewTest.setRotate(imgViewTest.getRotate() + 90);
-        System.out.println(imgViewTest.getRotate());
-    }
-    
 }
